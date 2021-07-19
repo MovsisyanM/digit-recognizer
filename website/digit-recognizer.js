d65d2a91b30bcdcf119dac7e4d48041458d18722
MovsisyanM <movsisyan@protonmail.com>
2021-07-20 00:51:35 +0400

    🔗 Linked endpoint

--- website/digit-recognizer.js
+++ website/digit-recognizer.js
@@ -11,19 +11,23 @@ let pathsX = [];
 let pathsY = [];
 
 
-
+// Init brush
 ctx.lineWidth = 13;
 ctx.lineCap = "round";
 ctx.strokeStyle = '#000000';
 ctx.fillStyle = '#000000';
 
+
+// Resize canvas to beautiful proportions
 function correctHeight() {
     if (holder.clientWidth < holder.clientHeight) {
         canvas.height = holder.clientWidth
         canvas.width = canvas.height
     } else {
-        canvas.height = holder.clientHeight
-        canvas.width = canvas.height
+        if (canvas.height !== holder.clientHeight) {
+            canvas.height = holder.clientHeight
+            canvas.width = canvas.height
+        }
     }
 }
 
@@ -35,6 +39,8 @@ function startDraw(e) {
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = "#000000";
 
+
+    // Reset arrays that record mouse movement
     pathsX = [];
     pathsY = [];
 
@@ -49,10 +55,12 @@ function startDraw(e) {
     let currX = e.pageX - canvas.offsetLeft;
     let currY = e.pageY - holder.offsetTop;
 
+
     maxX = currX;
     minX = currX;
     maxY = currY;
     minY = currY;
+
 
     // Start path
     ctx.moveTo(currX, currY)
@@ -63,12 +71,15 @@ function startDraw(e) {
 
 // Called when mouseup
 function endDraw(e) {
+    // Stop painting
     painting = false
     ctx.closePath()
 
+    // Get the width and height of the drawing (not the canvas)
     let width = (maxX - minX);
     let height = (maxY - minY);
 
+    // Calculate the offset of drawing center from canvas center 
     let offsetX = canvas.width/2 - (minX + width/2);
     let offsetY = canvas.height/2 - (minY + height/2);
 
@@ -77,12 +88,13 @@ function endDraw(e) {
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = "#000000";
 
+    // Init brush
     ctx.lineCap = "round";
     ctx.lineWidth = canvas.height/10;
     ctx.moveTo(0,0)
     ctx.beginPath()
 
-    // For each stroke, repaint it scaled
+    // For each stroke, repaint it centered
     for (let i = 0; i < pathsX.length; i++) {
         // Drawing the line
         ctx.lineTo(pathsX[i] + offsetX, pathsY[i] + offsetY);
@@ -90,23 +102,36 @@ function endDraw(e) {
     }
     
     ctx.closePath()
+
+
+    // Send data to endpoint
+    xhr = new XMLHttpRequest();
+    let rstring = "http://127.0.0.1:5000/";
+    xhr.open("POST", rstring, true);
+    xhr.send(canvas.toDataURL());
 }
 
+
+// Draw to the next mouse position
 function draw(e) {
     // If not drawing yet, idle
     if (!painting) return;
+
 
     // Setting brush size and cap
     ctx.lineCap = "round";
     ctx.lineWidth = canvas.height/10;
 
+
     // Getting current x, y coordinates relative to canvas top left
     let currX = e.pageX - canvas.offsetLeft;
     let currY = e.pageY - holder.offsetTop;
 
+
     // Drawing the line
     ctx.lineTo(currX, currY);
     ctx.stroke();
+
 
     // Set coordinate extrema
     if (currX > maxX) {
@@ -143,4 +168,11 @@ document.onkeydown = (e) => {
     }
 }
 
-correctHeight()
+
+// Update canvas size each frame
+function Run() {
+    correctHeight();
+    window.requestAnimationFrame(Run);
+}
+
+Run();
--- website/endpoint.py
+++ website/endpoint.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, request, Response
+import urllib
 app = Flask(__name__)
 
 
 @app.route("/", methods=["POST"])
 def hello():
-    return "Hello World!"
+    data = request.data.decode("utf-8")
+    print("received image data")
+    resp = Response(f'<img src="{data}"></img>')
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
 
 
 if __name__ == "__main__":
