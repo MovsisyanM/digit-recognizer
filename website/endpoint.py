e72495a6c8aba7c38cbf0ad2e4134131f73262f8
MovsisyanM <movsisyan@protonmail.com>
2021-07-20 14:38:12 +0400

    Data collection means and data

--- website/endpoint.py
+++ website/endpoint.py
@@ -1,14 +1,24 @@
 from flask import Flask, request, Response
 import urllib
+from PIL import Image, ImageOps
 app = Flask(__name__)
+
+imgCounter = 1015
 
 
 @app.route("/", methods=["POST"])
 def hello():
+    global imgCounter
     data = request.data.decode("utf-8")
-    print("received image data")
-    resp = Response(f'<img src="{data}"></img>')
+    print(f"received image data\n\n{imgCounter}\n\n")
+
+    img = Image.open(urllib.request.urlopen(data))
+    print(img.size)
+    img = ImageOps.grayscale(img.resize((28, 28)))
+    img.save(f"images/drawn{imgCounter}.png")
+    resp = Response(f'kk')
     resp.headers['Access-Control-Allow-Origin'] = '*'
+    imgCounter += 1
     return resp
 
 
